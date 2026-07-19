37420c5e7f94307aef3c2bab1be743c4d8f3cf89
Krishna Jayaraman <kjmn1999@gmail.com>
2026-07-19 00:59:30 -0400

    Add Parser Separator and add /data to gitignore

--- main.py
+++ main.py
@@ -1,9 +1,70 @@
+import argparse
 from src.collectors.fandom_allcollector import Fandom_All_Collector
+from src.collectors.fandom_manual_collector import ManualPageCollector
+
+DEFAULT_URL = "https://gunpla.fandom.com/api.php"
+
+
+def run_full_parse(url: str, mode: str):
+    collector = Fandom_All_Collector(url=url)
+    collector.fetch_all(mode=mode)
+
+def run_manual(url: str, output_file: str, pages: list[str]):
+    base_collector = Fandom_All_Collector(url=url)
+    manual_collector = ManualPageCollector(base_collector=base_collector, output_file=output_file)
+    manual_collector.append_manual_pages(pages)
+
 
 def main():
-    collector = Fandom_All_Collector(url="https://gunpla.fandom.com/api.php")
+    parser = argparse.ArgumentParser(description="Gunpla Data Pipeline")
+    subparser = parser.add_subparsers(dest="command", required=True)
 
-    collector.fetch_all(page_limit=20)
 
+    ## ----- FULL QUERY AND PARSE
+
+    full_parser = subparser.add_parser("full", help="Run full Wiki Data Collection")
+    full_parser.add_argument(
+        "--mode",
+        choices=["full", "discovery"],
+        default="full",
+        help="Full Collection or Discovery (Recommended first time to catch specific infobox names)"
+    )
+    full_parser.add_argument(
+        "--url",
+        default=DEFAULT_URL,
+        help="Fandom API Url"
+    )
+
+    ## ----- MANUAL INSERT COMMAND
+
+    manual_parser = subparser.add_parser("manual", help="Append exact page manually")
+    manual_parser.add_argument(
+        "--pages",
+        nargs="+",
+        required=True,
+        help="List of exact page names"
+    )
+
+    manual_parser.add_argument(
+        "--output",
+        required=True,
+        help="Output JSONL file"
+    )
+    manual_parser.add_argument(
+        "--url",
+        default=DEFAULT_URL,
+        help="Fandom API URL"
+    )
+
+    args = parser.parse_args()
+
+    if args.command == "full":
+        run_full_parse(url=args.url, mode=args.mode)
+    
+    elif args.command == "manual":
+        run_manual(url=args.url, output_file=args.output, pages=args.pages)     
+       
 if __name__ == "__main__":
     main()
+
+
--- src/collectors/fandom_allcollector.py
+++ src/collectors/fandom_allcollector.py
@@ -2,13 +2,22 @@ import requests
 import json
 from datetime import datetime
 from pathlib import Path
+from bs4 import BeautifulSoup
+from src.collectors.collector_util import Collector_Util
+from src.collectors.discovery_util import Discovery_Util
+from src.collectors.data_writer import Dataset_Writer
+
 
 class Fandom_All_Collector:
     def __init__(self, url: str):
         self.url = url
         self.session = requests.Session()
+        self.collector_util = Collector_Util(url=url)
+        self.discovery_util = Discovery_Util(url=url)
+        self.seen_infoboxes = set()
+        self.data_writer = Dataset_Writer()
 
-    def fetch_all(self, page_limit : int=None):
+    def fetch_all(self, page_limit : int=None, mode="full"):
         """
         Gets every page from the wiki via allpages
         fetch full page content
@@ -18,38 +27,61 @@ class Fandom_All_Collector:
 
         print(f"{Fandom_All_Collector} fetched {len(pages)} pages from {self.url}")
         for i, page in enumerate(pages):
-
+        
             title = page["title"]
 
             try:
                 data = self.fetch_page(title)
-                if self.skip_page(data):
-                    continue
-                self._save_raw_data(title, data)
-                print(f"[{(i+1)/len(pages)}] Fetched and Saved for {title}")
+                wikitext = data["parse"]["wikitext"]["*"]
 
+                if not wikitext:
+                    print(f"[PRE-PARSE SKIP] {title}: Skipping page due to missing wikitext")
+                    continue
+                
+                parsed_data = self.collector_util.parse_page(title, wikitext)
+                if self.skip_page(title, parsed_data):
+                    continue
+                if mode == "full":
+                    self.data_writer.append(parsed_data)
+                    #self._save_raw_data(title, data, parsed_data)
+                    #print(f"[{(i+1)/len(pages)}] Fetched and Saved for {title}")
+                elif mode == "discovery":
+                    infobox = self.discovery_util.extract_data(wikitext)
+                    #print(f"[{(i+1)/len(pages)}] Discovering infobox for {title}")
+                    if infobox:
+                        self.seen_infoboxes.add(infobox)
+                    else:
+                        print(f"[SKIP] {title}: No infobox found")
+                    
             except Exception as e:
                 print(f"[ERROR] {title}: {e}")
 
-
+        if mode == "discovery":
+            with open("data\infoboxes.json", "w") as f:
+                json.dump(sorted(self.seen_infoboxes), f, indent=2)
+                print("Adding all discovered infoboxes to data/infoboxes.json")
+                
     def _fetch_all_pages(self, page_limit: int=None):
         """
         Use MediaWiki AllPages API to get all pages from the wiki
         """
         pages = []
+        seen = set()
         keep_going = None
+        last_continue = None
+
         while True:
             params = {
                 "action": "query",
                 "format": "json",
                 "list": "allpages",
-                "redirects": 1,
-                "aplimit": 500,
-                "apnamespace": 0
+                "aplimit": "max",
+                "apnamespace": 0,
+                "apfilterredir": "nonredirects"
             }
 
             if keep_going:
-                params["accontinue"] = keep_going
+                params["apcontinue"] = keep_going
 
             response = self.session.get(self.url, params=params)
             response.raise_for_status()
@@ -57,17 +89,39 @@ class Fandom_All_Collector:
             data = response.json()
 
             batch = data.get("query", {}).get("allpages", [])
-            pages.extend(batch)
+            
+            # Detect stagnation:
+            print(f"[FETCH] batch size: {len(batch)}")
+
+            for page in batch:
+                title = page["title"].strip()
+                if title in seen:
+                    continue
+
+                seen.add(title)
+                pages.append(page)
 
             # Temporary block to prevent fetching too many for testing
-            if page_limit and len(pages) >= page_limit:
-                return pages[:page_limit]
+                if page_limit and len(pages) >= page_limit:
+                    return pages[:page_limit]
             
             continue_data = data.get("continue")
+            
             if not continue_data:
                 break
             
-            keep_going = continue_data.get("accontinue")
+            keep_going = continue_data.get("apcontinue")
+
+            if keep_going == last_continue:
+                print(f"[WARN] Potential infinite loop, breaking here")
+            
+            last_continue = keep_going
+
+            print(f"[CONTINUE] -> {keep_going}")
+
+
+        
+        print(f"[DONE] total pages fetched {len(pages)}")
         return pages
 
     def fetch_page(self, title: str):
@@ -76,50 +130,63 @@ class Fandom_All_Collector:
         """
 
         params = {
-            "action": "query",
+            "action": "parse",
             "format": "json",
-            "titles": title,
-            "prop": "revisions",
-            "redirects": 1,
-            "rvprop": "content",
-            "rvslots": "main"
+            "page": title,
+            "prop": "wikitext",
+            "redirects": 1
         }
 
         response = self.session.get(self.url, params=params)
         response.raise_for_status()
-
+        
         return response.json()
     
-    def _save_raw_data(self, title: str, data: dict) -> str:
+    def _save_raw_data(self, title: str, data: dict, parsed_data: dict) -> str:
         now = datetime.now().strftime("%Y-%m-%d")
 
-        output_dir = Path("data/raw") / "fandom" / now
-        output_dir.mkdir(parents=True, exist_ok=True)
+        raw_output_dir = Path("data/raw") / "fandom" / now
+        raw_output_dir.mkdir(parents=True, exist_ok=True)
+        parsed_output_dir = Path("data/parsed") / "fandom" / now
+        parsed_output_dir.mkdir(parents=True, exist_ok=True)
+
+        # I want to call a separate file to parse the data, and save the raw and parsed data separately
+        # infobox = parse_infobox(data) -> Parses the revision / slots / main content / * data and returns a dict of the infobox data
+
+
 
         safe_title = title.replace(" ", "_").replace("/", "_")
-        file_path = output_dir / f"{safe_title}.json"
+        file_path = raw_output_dir / f"{safe_title}.json"
 
         with open(file_path, "w", encoding="utf-8") as f:
             json.dump(data, f, ensure_ascii=False, indent=2)
         
+
+        if parsed_data:
+            parsed_path = parsed_output_dir / f"{safe_title}.json"
+            with open(parsed_path, "w", encoding="utf-8") as f:
+                json.dump(parsed_data, f, ensure_ascii=False, indent=2)
+
+
         return str(file_path)
     
 
-    def skip_page(self, json_data: dict) -> bool:
+    def skip_page(self, title, parsed_data: dict) -> bool:
         """
-        Check if the current page is a disambiguation page:
-        I.E. "Did you mean to search for one of these kits?
-        HOWEVER, there are instances where a page may be a disambiguation but still have product details (maybe the kit is discontinued or something)
-        """
-        content = None
-        pages = json_data.get("query", {}).get("pages", {}) # Technically there will only be one page at this point
-        for id, page in pages.items():
-            revisions = page.get("revisions")
-            if not revisions:
-                return True
-            content = revisions[0].get("slots", {}).get("main", {}).get("*")
+        IF ANY OF THE FOLLOWING IS TRUE:
+        - If the page doesn't have a category for gunpla or model kit
+        - The page doesn't have an infobox with related product data
+        - the parsed_data object is None
 
-        if not isinstance(content, str):
+        WE SKIP THE PAGE
+        """
+        if not parsed_data or not self.discovery_util.check_plamo_category(parsed_data.get("categories", [])) or not self.discovery_util.check_plamo_infobox(parsed_data):
+            print(f"[POST-PARSE SKIP] Skipping {title} due to failing criteria")
             return True
+        return False
         
-        return "{{plamo_infobox" not in content.lower()
+        
+        
+
+    
+
